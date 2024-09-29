097a2fe12aa7ff9f7ebe4d67bfcab6896e2abacc
Adam Fritz <62766637+hammahutz@users.noreply.github.com>
2024-09-29 19:01:10 +0200

    Revert "Work in progress"

--- Mania.Core/ContentPaths.cs
+++ Mania.Core/ContentPaths.cs
@@ -2,10 +2,32 @@ public static class ContentPaths
 {
 	public static class Json
 	{
-		public const string Example = @"Json\Example";
+		public const string Example = @"\Json\Example";
+	}
+	public static class Sound
+	{
+		public static class Creep
+		{
+			public const string Attack = @"\Sound\Creep\Attack";
+			public const string Hurt = @"\Sound\Creep\Hurt";
+		}
+		public static class Hero
+		{
+			public const string Attack = @"\Sound\Hero\Attack";
+		}
 	}
 	public static class SpriteFont
 	{
-		public const string Debug = @"SpriteFont\Debug";
+		public const string Debug = @"\SpriteFont\Debug";
+	}
+	public static class Texture2D
+	{
+		public static class Hero
+		{
+			public const string Attack1 = @"\Texture2D\Hero\Attack1";
+			public const string Attack2 = @"\Texture2D\Hero\Attack2";
+			public const string Jumping = @"\Texture2D\Hero\Jumping";
+			public const string Running = @"\Texture2D\Hero\Running";
+		}
 	}
 }
--- Mania.Engine/Scene.cs
+++ Mania.Engine/Scene.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using Mania.Engine.GameActors;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -14,37 +12,23 @@ public abstract class Scene
     protected ContentManager GlobalContent { get; private set; }
     protected ContentManager LocalContent { get; private set; }
 
-    protected List<Actor> Actors { get; set; }
-
     public void Enter(Game game)
     {
         GlobalContent = game.Content;
         LocalContent = new ContentManager(game.Services);
         LocalContent.RootDirectory = "Content";
-        Actors = new List<Actor>();
         LoadContent();
     }
 
     protected abstract void LoadContent();
-    public void Update(GameTime gameTime)
-    {
-        Actors.ForEach(a => a.UpdateActor(gameTime));
-        UpdateScene(gameTime);
-    }
-    public void Draw(SpriteBatch spriteBatch)
-    {
-        Actors.ForEach(a => a.DrawActor(spriteBatch));
-        DrawScene(spriteBatch);
-    }
-    public virtual void UpdateScene(GameTime gameTime) { }
-    public virtual void DrawScene(SpriteBatch spriteBatch) { }
+    public abstract void Update(GameTime gameTime);
+    public abstract void Draw(SpriteBatch spriteBatch);
     protected abstract void UnloadContent();
 
     public void Exit()
     {
         LocalContent.Unload();
         LocalContent.Dispose();
-        Actors.ForEach(a => a = null);
         UnloadContent();
     }
 }
