39a38ccb197892926ff39cd7056c43e0fef219a3
hammahutz <62766637+hammahutz@users.noreply.github.com>
2024-10-04 10:23:00 +0200

    WIP: SpriteComponent

--- Mania.Engine/GameLogic/Components/Sprites/SpriteComponent.cs
+++ Mania.Engine/GameLogic/Components/Sprites/SpriteComponent.cs
@@ -6,45 +6,32 @@ namespace Mania.Engine.GameLogic.Components;
 
 public class SpriteComponent : GfxComponent
 {
-    private int _currentRow, _currentColumn;
-    public Point SpriteSize
-    {
-        get => (SourceRectangle?.Size) ?? Point.Zero;
-        set => SourceRectangle = SourceRectangle is not null ? new Rectangle(SourceRectangle.Value.Location, value) : new Rectangle(Point.Zero, value);
-    }
-    public Point SpritePosition => new(CurrentRow * SpriteSize.X, CurrentColumn * SpriteSize.Y);
+    public Point Size { get; private set; }
+    public Point Location => new(CurrentRow * Size.X, CurrentColumn * Size.Y);
 
-    public int CurrentRow
-    {
-        get => _currentRow;
-        set
-        {
-            _currentRow = Math.Clamp(value, 0, AtlasRows);
-            SourceRectangle = new Rectangle(SpritePosition, SpriteSize);
-        }
-    }
+    public int CurrentRow { get; private set; } = 0;
+    public int CurrentColumn { get; private set; } = 0;
 
-    public int CurrentColumn
-    {
-        get => CurrentColumn;
-        set => Math.Clamp(value, 0, AtlasColumns);
-    }
+    public int Rows => Size.X / Texture2D.Width;
+    public int Columns => Size.Y / Texture2D.Height;
 
-    public int AtlasRows => SpriteSize.X / Texture2D.Width;
-    public int AtlasColumns => SpriteSize.Y / Texture2D.Height;
-
-
-
-    public SpriteComponent(Node node, Texture2D texture2D)
+    public SpriteComponent(Node node, Texture2D texture2D, Point spriteSize)
         : base(node, texture2D)
     {
-        CurrentRow = 0;
-
-        CurrentColumn = 0;
-        SourceRectangle = new Rectangle(0, 0, 0, 0);
+        SetLocation(Point.Zero);
+        SetSize(spriteSize);
     }
 
-    public void SetAtlasPosition(int row, int column)
+    public void SetLocation(Point location)
     {
+        CurrentRow = Math.Clamp(location.X, 0, Rows);
+        CurrentColumn = Math.Clamp(location.Y, 0, Columns);
+        SourceRectangle = new Rectangle(Location, Size);
+    }
+
+    public void SetSize(Point size)
+    {
+        Size = size;
+        SourceRectangle = new Rectangle(Location, Size);
     }
 }
